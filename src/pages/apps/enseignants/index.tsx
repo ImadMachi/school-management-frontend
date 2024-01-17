8396c22a7ab6a28fca6ca57b9fd97050408ab266
AchrafGitKh <kharbabachraf@gmail.com>
2024-01-17 01:19:33 +0100

    teacher management front end

---
+++ src/pages/apps/enseignants/index.tsx
@@ -0,0 +1,312 @@
+// ** React Imports
+import { useState, useEffect, MouseEvent, useCallback } from 'react'
+
+// ** Next Imports
+import Link from 'next/link'
+import { GetStaticProps, InferGetStaticPropsType } from 'next/types'
+
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import Menu from '@mui/material/Menu'
+import Grid from '@mui/material/Grid'
+import { DataGrid } from '@mui/x-data-grid'
+import { styled } from '@mui/material/styles'
+import MenuItem from '@mui/material/MenuItem'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import CardHeader from '@mui/material/CardHeader'
+import InputLabel from '@mui/material/InputLabel'
+import FormControl from '@mui/material/FormControl'
+import CardContent from '@mui/material/CardContent'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+
+// ** Store Imports
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState, AppDispatch } from 'src/store'
+
+
+// ** Custom Components Imports
+import CustomAvatar from 'src/@core/components/mui/avatar'
+
+// ** Utils Import
+import { getInitials } from 'src/@core/utils/get-initials'
+
+// ** Actions Imports
+import { fetchData, deleteTeacher } from 'src/store/apps/teachers'
+
+// ** Types Imports
+import { TeachersType } from 'src/types/apps/teachers'
+
+// ** Custom Table Components Imports
+import { useAuth } from 'src/hooks/useAuth'
+import TableHeader from 'src/views/apps/teacher/list/TableHeader'
+import AddTeacherDrawer from 'src/views/apps/teacher/list/AddTeacherDrawer'
+
+interface CellType {
+  row: TeachersType
+}
+
+const StyledLink = styled(Link)(({ theme }) => ({
+  fontWeight: 600,
+  fontSize: '1rem',
+  cursor: 'pointer',
+  textDecoration: 'none',
+  color: theme.palette.text.secondary,
+  '&:hover': {
+    color: theme.palette.primary.main
+  }
+}))
+
+// ** renders client column
+const renderClient = (row: TeachersType) => {
+  return (
+    <CustomAvatar skin='light' color={'primary'} sx={{ mr: 3, width: 30, height: 30, fontSize: '.875rem' }}>
+      {getInitials(`${row.firstName} ${row.lastName}`)}
+    </CustomAvatar>
+  )
+}
+
+const RowOptions = ({ id }: { id: number }) => {
+  // ** Hooks
+  const dispatch = useDispatch<AppDispatch>()
+  const auth = useAuth()
+
+  // ** State
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+  const rowOptionsOpen = Boolean(anchorEl)
+
+  const handleRowOptionsClick = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleRowOptionsClose = () => {
+    setAnchorEl(null)
+  }
+
+  const handleDelete = () => {
+    dispatch(deleteTeacher({ id, headers: { Authorization: `Bearer ${auth.accessToken}` } }))
+    handleRowOptionsClose()
+  }
+
+  return (
+    <>
+      <IconButton size='small' onClick={handleRowOptionsClick}>
+        <Icon icon='mdi:dots-vertical' />
+      </IconButton>
+      <Menu
+        keepMounted
+        anchorEl={anchorEl}
+        open={rowOptionsOpen}
+        onClose={handleRowOptionsClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right'
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right'
+        }}
+        PaperProps={{ style: { minWidth: '8rem' } }}
+      >
+        <MenuItem
+          component={Link}
+          sx={{ '& svg': { mr: 2 } }}
+          onClick={handleRowOptionsClose}
+          href='/apps/user/view/overview/'
+        >
+          <Icon icon='mdi:eye-outline' fontSize={20} />
+          Voir
+        </MenuItem>
+        <MenuItem onClick={handleRowOptionsClose} sx={{ '& svg': { mr: 2 } }}>
+          <Icon icon='mdi:pencil-outline' fontSize={20} />
+          Modifier
+        </MenuItem>
+        <MenuItem onClick={handleDelete} sx={{ '& svg': { mr: 2 } }}>
+          <Icon icon='mdi:delete-outline' fontSize={20} />
+          Supprimer
+        </MenuItem>
+      </Menu>
+    </>
+  )
+}
+
+const columns = [
+  {
+    flex: 0.2,
+    minWidth: 230,
+    headerName: 'enseignant',
+    field: 'fullName',
+    renderCell: ({ row }: CellType) => {
+      const { firstName, lastName } = row
+
+      return (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          {renderClient(row)}
+          <Box sx={{ display: 'flex', alignItems: 'flex-start', flexDirection: 'column' }}>
+            <StyledLink href='/apps/user/view/overview/'>
+              {firstName} {lastName}
+            </StyledLink>
+          </Box>
+        </Box>
+      )
+    }
+  },
+  {
+    flex: 0.15,
+    minWidth: 120,
+    headerName: 'num tel',
+    field: 'phoneNumber',
+    renderCell: ({ row }: CellType) => {
+      return (
+        <Typography noWrap sx={{ textTransform: 'capitalize' }}>
+          {row.phoneNumber}
+        </Typography>
+      )
+    }
+  },
+  {
+    flex: 0.15,
+    minWidth: 120,
+    headerName: 'Date de Naissance',
+    field: 'dateOfBirth',
+    renderCell: ({ row }: CellType) => (
+      <Typography noWrap>{row.dateOfBirth ? new Date(row.dateOfBirth).toLocaleDateString() : '-'}</Typography>
+    ),
+  },
+  {
+    flex: 0.15,
+    minWidth: 120,
+    headerName: 'Date d\'Embauche',
+    field: 'dateOfHiring',
+    renderCell: ({ row }: CellType) => (
+      <Typography noWrap>{row.dateOfHiring ? new Date(row.dateOfHiring).toLocaleDateString() : '-'}</Typography>
+    ),
+  },
+  {
+    flex: 0.1,
+    minWidth: 90,
+    sortable: false,
+    field: 'sex',
+    headerName: 'Sexe',
+    renderCell: ({ row }: CellType) => <Typography noWrap>{row.sexe || '-'}</Typography>,
+  },
+  {
+    flex: 0.15,
+    minWidth: 120,
+    headerName: 'Possède Compte',
+    field: 'hasAccount',
+    renderCell: ({ row }: CellType) => {
+      return (
+        <Typography noWrap sx={{ textTransform: 'capitalize' }}>
+          {!!row.user ? 'Oui' : 'Non'}
+        </Typography>
+      )
+    }
+  },
+  {
+    flex: 0.1,
+    minWidth: 90,
+    sortable: false,
+    field: 'actions',
+    headerName: 'Actions',
+    renderCell: ({ row }: CellType) => <RowOptions id={row.id} />
+  }
+];
+
+
+const UserList = () => {
+  // ** State
+  const [plan, setPlan] = useState<string>('')
+  const [value, setValue] = useState<string>('')
+  const [pageSize, setPageSize] = useState<number>(10)
+  const [addUserOpen, setAddUserOpen] = useState<boolean>(false)
+
+  // ** Hooks
+  const dispatch = useDispatch<AppDispatch>()
+  const store = useSelector((state: RootState) => state.teachers)
+  const auth = useAuth()
+
+  useEffect(() => {
+    dispatch(
+      fetchData({
+        params: {
+          q: value
+        },
+        headers: {
+          Authorization: `Bearer ${auth.accessToken}`
+        }
+      })
+    )
+  }, [dispatch, plan, value])
+
+  const handleFilter = useCallback((val: string) => {
+    setValue(val)
+  }, [])
+
+  const toggleAddUserDrawer = () => setAddUserOpen(!addUserOpen)
+
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Card>
+          {/* <CardHeader title='Search Filters' />
+          <CardContent>
+            <Grid container spacing={6}>
+              <Grid item sm={4} xs={12}>
+                <FormControl fullWidth>
+                  <InputLabel id='plan-select'>Select Plan</InputLabel>
+                  <Select
+                    fullWidth
+                    value={plan}
+                    id='select-plan'
+                    label='Select Plan'
+                    labelId='plan-select'
+                    onChange={handlePlanChange}
+                    inputProps={{ placeholder: 'Select Plan' }}
+                  >
+                    <MenuItem value=''>Select Plan</MenuItem>
+                    <MenuItem value='basic'>Basic</MenuItem>
+                    <MenuItem value='company'>Company</MenuItem>
+                    <MenuItem value='enterprise'>Enterprise</MenuItem>
+                    <MenuItem value='team'>Team</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+          </CardContent> */}
+          {/* <Divider /> */}
+          <TableHeader value={value} handleFilter={handleFilter} toggle={toggleAddUserDrawer} />
+          <DataGrid
+            autoHeight
+            rows={store.data}
+            columns={columns}
+            checkboxSelection
+            pageSize={pageSize}
+            disableSelectionOnClick
+            rowsPerPageOptions={[10, 25, 50]}
+            onPageSizeChange={(newPageSize: number) => setPageSize(newPageSize)}
+          />
+        </Card>
+      </Grid>
+
+      <AddTeacherDrawer open={addUserOpen} toggle={toggleAddUserDrawer} />
+    </Grid>
+  )
+}
+
+// export const getStaticProps: GetStaticProps = async () => {
+//   const res = await axios.get('/cards/statistics')
+//   const apiData: CardStatsType = res.data
+
+//   return {
+//     props: {
+//       apiData
+//     }
+//   }
+// }
+
+export default UserList
---
+++ src/views/apps/teacher/list/AddTeacherDrawer.tsx
@@ -0,0 +1,277 @@
+// ** React Imports
+import { useState } from 'react'
+
+// ** MUI Imports
+import Drawer from '@mui/material/Drawer'
+import Select from '@mui/material/Select'
+import Button from '@mui/material/Button'
+import MenuItem from '@mui/material/MenuItem'
+import { styled } from '@mui/material/styles'
+import TextField from '@mui/material/TextField'
+import IconButton from '@mui/material/IconButton'
+import InputLabel from '@mui/material/InputLabel'
+import Typography from '@mui/material/Typography'
+import Box, { BoxProps } from '@mui/material/Box'
+import FormControl from '@mui/material/FormControl'
+import FormHelperText from '@mui/material/FormHelperText'
+
+// ** Third Party Imports
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm, Controller } from 'react-hook-form'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+
+// ** Store Imports
+import { useDispatch } from 'react-redux'
+
+// ** Actions Imports
+import { addTeacher } from 'src/store/apps/teachers'
+
+// ** Types Imports
+import { AppDispatch } from 'src/store'
+
+interface SidebarAddUserType {
+  open: boolean
+  toggle: () => void
+}
+
+interface UserData {
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  dateOfBirth: Date;
+  dateOfHiring: Date;
+  sexe: string;
+}
+
+
+const showErrors = (field: string, valueLen: number, min: number) => {
+  if (valueLen === 0) {
+    return `${field} field is required`
+  } else if (valueLen > 0 && valueLen < min) {
+    return `${field} must be at least ${min} characters`
+  } else {
+    return ''
+  }
+}
+
+const Header = styled(Box)<BoxProps>(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(3, 4),
+  justifyContent: 'space-between',
+  backgroundColor: theme.palette.background.default
+}))
+
+const schema = yup.object().shape({
+  firstName: yup.string().min(3).required(),
+  lastName: yup.string().min(3).required(),
+  phoneNumber: yup.string().required(),
+  dateOfBirth: yup.date().required(),
+  dateOfHiring: yup.date().required(),
+  sexe: yup.string().required(),
+});
+
+const defaultValues: UserData = {
+  firstName: '',
+  lastName: '',
+  phoneNumber: '',
+  dateOfBirth: new Date(),
+  dateOfHiring: new Date(),
+  sexe: '',
+};
+
+const AddTeacherDrawer = (props: SidebarAddUserType) => {
+  // ** Props
+  const { open, toggle } = props
+
+  // ** Hooks
+  const dispatch = useDispatch<AppDispatch>()
+  const {
+    reset,
+    control,
+    setValue,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    defaultValues,
+    mode: 'onChange',
+    resolver: yupResolver(schema)
+  })
+
+  const onSubmit = (data: UserData) => {
+    dispatch(addTeacher({ ...data }) as any)
+    toggle()
+    reset()
+  }
+
+  const handleClose = () => {
+    toggle()
+    reset()
+  }
+
+  return (
+    <Drawer
+      open={open}
+      anchor='right'
+      variant='temporary'
+      onClose={handleClose}
+      ModalProps={{ keepMounted: true }}
+      sx={{ '& .MuiDrawer-paper': { width: { xs: 300, sm: 400 } } }}
+    >
+      <Header>
+        <Typography variant='h6'>Ajouter Enseignant</Typography>
+        <IconButton size='small' onClick={handleClose} sx={{ color: 'text.primary' }}>
+          <Icon icon='mdi:close' fontSize={20} />
+        </IconButton>
+      </Header>
+      <Box sx={{ p: 5 }}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl fullWidth sx={{ mb: 6 }}>
+            <Controller
+              name='firstName'
+              control={control}
+              rules={{ required: true }}
+              render={({ field: { value, onChange } }) => (
+                <TextField
+                  value={value}
+                  label='Prénom'
+                  onChange={onChange}
+                  placeholder='John'
+                  error={Boolean(errors.firstName)}
+                />
+              )}
+            />
+            {errors.firstName && (
+              <FormHelperText sx={{ color: 'error.main' }}>{errors.firstName.message}</FormHelperText>
+            )}
+          </FormControl>
+          <FormControl fullWidth sx={{ mb: 6 }}>
+            <Controller
+              name='lastName'
+              control={control}
+              rules={{ required: true }}
+              render={({ field: { value, onChange } }) => (
+                <TextField
+                  value={value}
+                  label='Nom'
+                  onChange={onChange}
+                  placeholder='Doe'
+                  error={Boolean(errors.lastName)}
+                />
+              )}
+            />
+            {errors.lastName && <FormHelperText sx={{ color: 'error.main' }}>{errors.lastName.message}</FormHelperText>}
+          </FormControl>
+          <FormControl fullWidth sx={{ mb: 6 }}>
+            <Controller
+              name='phoneNumber'
+              control={control}
+              rules={{ required: true }}
+              render={({ field: { value, onChange } }) => (
+                <TextField
+                  value={value}
+                  label='Num Tel'
+                  onChange={onChange}
+                  placeholder='+212 612-345678'
+                  error={Boolean(errors.phoneNumber)}
+                />
+              )}
+            />
+            {errors.phoneNumber && (
+              <FormHelperText sx={{ color: 'error.main' }}>{errors.phoneNumber.message}</FormHelperText>
+            )}
+          </FormControl>
+          <FormControl fullWidth sx={{ mb: 6 }}>
+            <Controller
+              name="dateOfBirth"
+              control={control}
+              rules={{ required: true }}
+              render={({ field: { value, onChange } }) => (
+                <TextField
+                  type="date"
+                  value={value}
+                  onChange={onChange}
+                  label="Date de Naissance"
+                  error={Boolean(errors.dateOfBirth)}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              )}
+            />
+            {errors.dateOfBirth && (
+              <FormHelperText sx={{ color: 'error.main' }}>
+                {errors.dateOfBirth.message}
+              </FormHelperText>
+            )}
+          </FormControl>
+
+          <FormControl fullWidth sx={{ mb: 6 }}>
+            <Controller
+              name="dateOfHiring"
+              control={control}
+              rules={{ required: true }}
+              render={({ field: { value, onChange } }) => (
+                <TextField
+                  type="date"
+                  value={value}
+                  onChange={onChange}
+                  label="Date d'embauche"
+                  error={Boolean(errors.dateOfHiring)}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              )}
+            />
+            {errors.dateOfHiring && (
+              <FormHelperText sx={{ color: 'error.main' }}>
+                {errors.dateOfHiring.message}
+              </FormHelperText>
+            )}
+          </FormControl>
+
+          <FormControl fullWidth sx={{ mb: 6 }}>
+            <InputLabel htmlFor="sexe">Sexe</InputLabel>
+            <Controller
+              name="sexe"
+              control={control}
+              rules={{ required: true }}
+              render={({ field: { value, onChange } }) => (
+                <Select
+                  id="sexe"
+                  value={value}
+                  onChange={(e) => onChange(e.target.value)}
+                  label="Sexe"
+                  error={Boolean(errors.sexe)}
+                >
+                  <MenuItem value="male">Masculin</MenuItem>
+                  <MenuItem value="female">Féminin</MenuItem>
+                  {/* Add other options if needed */}
+                </Select>
+              )}
+            />
+            {errors.sexe && (
+              <FormHelperText sx={{ color: 'error.main' }}>{errors.sexe.message}</FormHelperText>
+            )}
+          </FormControl>
+
+
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Button size='large' type='submit' variant='contained' sx={{ mr: 3 }}>
+              Soumettre
+            </Button>
+            <Button size='large' variant='outlined' color='secondary' onClick={handleClose}>
+              Annuler
+            </Button>
+          </Box>
+        </form>
+      </Box>
+    </Drawer>
+  )
+}
+
+export default AddTeacherDrawer;

dd26b9d8c393ad9ae48d29a8ba159d01f2adb69d
AchrafKhGit <kharbabachraf@gmail.com>
2024-02-20 02:26:06 +0100

    new modification for message apearence

--- src/pages/apps/etudiants/overview/[folder].tsx
+++ src/pages/apps/etudiants/overview/[folder].tsx
@@ -354,9 +354,11 @@ const UserViewLeft = () => {
             <UserSubscriptionDialog open={subscriptionDialogOpen} setOpen={setSubscriptionDialogOpen} /> */}
           </Card>
         </Grid>
+        {userData.userId !== null && (
         <Grid item xs={12} md={7} sx={{ display: 'flex' }} >
           <EmailAppLayout folder={folder as string} />
         </Grid>
+        )}
       </Grid >
     )
   } else {

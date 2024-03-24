a0a9cb761b6ea535eecb79982eed262245e44607
AchrafKhGit <kharbabachraf@gmail.com>
2024-03-24 17:59:22 +0000

    profile page  + some modifications

--- src/views/apps/profile/UserProfile.tsx
+++ src/views/apps/profile/UserProfile.tsx
@@ -5,24 +5,18 @@ import { useState, useEffect, ReactElement, SyntheticEvent } from 'react'
 import { useRouter } from 'next/router'
 
 // ** MUI Components
-import Tab from '@mui/material/Tab'
-import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
-import TabPanel from '@mui/lab/TabPanel'
-import TabContext from '@mui/lab/TabContext'
-import Typography from '@mui/material/Typography'
+
 import { styled, Theme } from '@mui/material/styles'
 import useMediaQuery from '@mui/material/useMediaQuery'
 import MuiTabList, { TabListProps } from '@mui/lab/TabList'
-import CircularProgress from '@mui/material/CircularProgress'
 
 // ** Icon Imports
-import Icon from 'src/@core/components/icon'
 
 // ** Demo Components
 import UserProfileHeader from 'src/views/apps/profile/UserProfileHeader'
-import { UserDataType } from 'src/context/types'
 import { UserType } from 'src/types/apps/UserType'
+import AboutOverivew from './AboutOverivew'
 
 const TabList = styled(MuiTabList)<TabListProps>(({ theme }) => ({
   '& .MuiTabs-indicator': {
@@ -73,71 +67,10 @@ const UserProfile = ({ data }: { data: UserType | null}) => {
       <Grid item xs={12}>
         <UserProfileHeader />
       </Grid>
-      {/* {activeTab === undefined ? null : (
-        <Grid item xs={12}>
-          <TabContext value={activeTab}>
-            <Grid container spacing={6}>
-              <Grid item xs={12}>
-                <TabList
-                  variant='scrollable'
-                  scrollButtons='auto'
-                  onChange={handleChange}
-                  aria-label='customized tabs example'
-                >
-                  <Tab
-                    value='profile'
-                    label={
-                      <Box sx={{ display: 'flex', alignItems: 'center', ...(!hideText && { '& svg': { mr: 2 } }) }}>
-                        <Icon fontSize={20} icon='mdi:account-outline' />
-                        {!hideText && 'Profile'}
-                      </Box>
-                    }
-                  />
-                  <Tab
-                    value='teams'
-                    label={
-                      <Box sx={{ display: 'flex', alignItems: 'center', ...(!hideText && { '& svg': { mr: 2 } }) }}>
-                        <Icon fontSize={20} icon='mdi:account-multiple-outline' />
-                        {!hideText && 'Teams'}
-                      </Box>
-                    }
-                  />
-                  <Tab
-                    value='projects'
-                    label={
-                      <Box sx={{ display: 'flex', alignItems: 'center', ...(!hideText && { '& svg': { mr: 2 } }) }}>
-                        <Icon fontSize={20} icon='mdi:view-grid-outline' />
-                        {!hideText && 'Projects'}
-                      </Box>
-                    }
-                  />
-                  <Tab
-                    value='connections'
-                    label={
-                      <Box sx={{ display: 'flex', alignItems: 'center', ...(!hideText && { '& svg': { mr: 2 } }) }}>
-                        <Icon fontSize={20} icon='mdi:link' />
-                        {!hideText && 'Connections'}
-                      </Box>
-                    }
-                  />
-                </TabList>
-              </Grid>
-              <Grid item xs={12} sx={{ pt: theme => `${theme.spacing(4)} !important` }}>
-                {isLoading ? (
-                  <Box sx={{ mt: 6, display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-                    <CircularProgress sx={{ mb: 4 }} />
-                    <Typography>Loading...</Typography>
-                  </Box>
-                ) : (
-                  <TabPanel sx={{ p: 0 }} value={activeTab}>
-                    {tabContentList[activeTab]}
-                  </TabPanel>
-                )}
-              </Grid>
-            </Grid>
-          </TabContext>
-        </Grid>
-      )} */}
+         <Grid item xs={12}>
+        <AboutOverivew/>
+      </Grid>
+     
     </Grid>
   )
 }
--- src/views/apps/profile/UserProfileHeader.tsx
+++ src/views/apps/profile/UserProfileHeader.tsx
@@ -1,90 +1,153 @@
 // ** React Imports
-import { useState, useEffect } from 'react'
+import { useState, useEffect, ChangeEvent } from "react";
 
 // ** MUI Components
-import Box from '@mui/material/Box'
-import Card from '@mui/material/Card'
-import Button from '@mui/material/Button'
-import { styled } from '@mui/material/styles'
-import CardMedia from '@mui/material/CardMedia'
-import Typography from '@mui/material/Typography'
-import CardContent from '@mui/material/CardContent'
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import CardContent from "@mui/material/CardContent";
 
 // ** Third Party Imports
-import axios from 'axios'
+import axios from "axios";
 
 // ** Icon Imports
-import Icon from 'src/@core/components/icon'
+import Icon from "src/@core/components/icon";
 
 // ** Types
-import { UserDataType } from 'src/context/types' 
-import { useAuth } from 'src/hooks/useAuth'
+import { UserDataType } from "src/context/types";
+import { useAuth } from "src/hooks/useAuth";
+import { uploadProfileImage } from "src/store/apps/users";
+import { AnyAction, ThunkDispatch } from "@reduxjs/toolkit";
+import { RootState } from "src/store";
+import { useDispatch, useSelector } from "react-redux";
 
-const ProfilePicture = styled('img')(({ theme }) => ({
+const ProfilePicture = styled("img")(({ theme }) => ({
   width: 140,
   height: 120,
   borderRadius: theme.shape.borderRadius,
   border: `5px solid ${theme.palette.common.white}`,
-  [theme.breakpoints.down('md')]: {
-    marginBottom: theme.spacing(4)
-  }
-}))
+  [theme.breakpoints.down("md")]: {
+    marginBottom: theme.spacing(4),
+  },
+}));
 
 const UserProfileHeader = () => {
   // ** State
-  const data = useAuth()
+  const { user, logout } = useAuth();
+  const userStore = useSelector((state: RootState) => state.users);
 
-  return data !== null ? (
+  const dispatch: ThunkDispatch<RootState, any, AnyAction> = useDispatch();
+
+  const userId = user?.id;
+  const [userIdData, setUserIdData] = useState<UserDataType | null>(null);
+
+  const handleLogout = () => {
+    logout();
+  };
+
+  const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+
+      try {
+        const response = await dispatch(
+          uploadProfileImage({ id: userId!, file })
+        ).unwrap();
+
+        console.log("Profile image uploaded successfully:", response);
+
+        if (user) {
+          const imageUrl = response.profileImage;
+          setUserIdData({ ...userIdData, profileImage: imageUrl });
+        }
+      } catch (error) {
+        console.error("Error uploading profile image:", error);
+      }
+      e.target.value = "";
+    }
+  };
+  useEffect(() => {
+    // Update state when the data is updated
+    if (userStore.data && userStore.data.length > 0) {
+      setUserIdData(userStore.data[0]);
+    }
+  }, [userStore.data]);
+  return user !== null ? (
     <Card>
       <CardMedia
-        component='img'
-        alt='profile-header'
-        image = '/images/cover.jpg'
+        component="img"
+        alt="profile-header"
+        image="/images/cover-page.png"
         sx={{
-          height: { xs: 150, md: 250 }
+          height: { xs: 150, md: 250 },
         }}
       />
       <CardContent
         sx={{
           pt: 0,
           mt: -8,
-          display: 'flex',
-          alignItems: 'flex-end',
-          flexWrap: { xs: 'wrap', md: 'nowrap' },
-          justifyContent: { xs: 'center', md: 'flex-start' }
+          display: "flex",
+          alignItems: "flex-end",
+          flexWrap: { xs: "wrap", md: "nowrap" },
+          justifyContent: { xs: "center", md: "flex-start" },
         }}
       >
-        <ProfilePicture src={`http://localhost:8000/uploads/${data?.user?.profileImage}`} alt='profile-picture' />
+        <ProfilePicture
+          src={`http://localhost:8000/uploads/${user?.profileImage}`}
+          alt="profile-picture"
+        />
         <Box
           sx={{
-            width: '100%',
-            display: 'flex',
+            width: "100%",
+            display: "flex",
             ml: { xs: 0, md: 6 },
-            alignItems: 'flex-end',
-            flexWrap: ['wrap', 'nowrap'],
-            justifyContent: ['center', 'space-between']
+            alignItems: "flex-end",
+            flexWrap: ["wrap", "nowrap"],
+            justifyContent: ["center", "space-between"],
           }}
         >
-          <Box sx={{ mb: [6, 0], display: 'flex', flexDirection: 'column', alignItems: ['center', 'flex-start'] }}>
-            <Typography variant='h5' sx={{ mb: 4 }}>
-              {data.user?.userData?.firstName} {data.user?.userData?.lastName}
+          <Box
+            sx={{
+              mb: [6, 0],
+              display: "flex",
+              flexDirection: "column",
+              alignItems: ["center", "flex-start"],
+            }}
+          >
+            <Typography variant="h5" sx={{ mb: 4 }}>
+              {user?.userData?.firstName} {user?.userData?.lastName}
             </Typography>
             <Box
               sx={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                justifyContent: ['center', 'flex-start']
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: ["center", "flex-start"],
               }}
-            >
-            </Box>
+            ></Box>
           </Box>
-          <Button variant='contained' startIcon={<Icon icon='mdi:account-check-outline' fontSize={20} />}>
-            Connected
-          </Button>
+          <Box sx={{ display: "flex", flexDirection: "row", gap: "10px" }}>
+            <Button
+              variant="contained"
+              startIcon={<Icon icon="mdi:gear" fontSize={20} />}
+            >
+              Modfifier les information
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              startIcon={<Icon icon="mdi:logout" fontSize={20} />}
+              onClick={handleLogout}
+            >
+              Se Déconnecter
+            </Button>
+          </Box>
         </Box>
       </CardContent>
     </Card>
-  ) : null
-}
+  ) : null;
+};
 
-export default UserProfileHeader
+export default UserProfileHeader;

2e7ac435deef8ad07ca3b3ace76eda0e3cf08fc7
Imad <machii.imad@gmail.com>
2024-05-13 11:46:23 +0100

    Fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -63,9 +63,6 @@ import { Provider } from "react-redux";
 import { store } from "src/store";
 import "../configs/axios-interceptors";
 
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-
 // ** Extend App Props with Emotion
 type ExtendedAppProps = AppProps & {
   Component: NextPage;
@@ -138,38 +135,36 @@ const App = (props: ExtendedAppProps) => {
           />
           <meta name="viewport" content="initial-scale=1, width=device-width" />
         </Head>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <AuthProvider>
-            <SettingsProvider
-              {...(setConfig ? { pageSettings: setConfig() } : {})}
-            >
-              <SettingsConsumer>
-                {({ settings }) => {
-                  return (
-                    <ThemeComponent settings={settings}>
-                      <WindowWrapper>
-                        <Guard authGuard={authGuard} guestGuard={guestGuard}>
-                          <AclGuard
-                            aclAbilities={aclAbilities}
-                            guestGuard={guestGuard}
-                          >
-                            {getLayout(<Component {...pageProps} />)}
-                          </AclGuard>
-                        </Guard>
-                      </WindowWrapper>
-                      <ReactHotToast>
-                        <Toaster
-                          position={settings.toastPosition}
-                          toastOptions={{ className: "react-hot-toast" }}
-                        />
-                      </ReactHotToast>
-                    </ThemeComponent>
-                  );
-                }}
-              </SettingsConsumer>
-            </SettingsProvider>
-          </AuthProvider>
-        </LocalizationProvider>
+        <AuthProvider>
+          <SettingsProvider
+            {...(setConfig ? { pageSettings: setConfig() } : {})}
+          >
+            <SettingsConsumer>
+              {({ settings }) => {
+                return (
+                  <ThemeComponent settings={settings}>
+                    <WindowWrapper>
+                      <Guard authGuard={authGuard} guestGuard={guestGuard}>
+                        <AclGuard
+                          aclAbilities={aclAbilities}
+                          guestGuard={guestGuard}
+                        >
+                          {getLayout(<Component {...pageProps} />)}
+                        </AclGuard>
+                      </Guard>
+                    </WindowWrapper>
+                    <ReactHotToast>
+                      <Toaster
+                        position={settings.toastPosition}
+                        toastOptions={{ className: "react-hot-toast" }}
+                      />
+                    </ReactHotToast>
+                  </ThemeComponent>
+                );
+              }}
+            </SettingsConsumer>
+          </SettingsProvider>
+        </AuthProvider>
       </CacheProvider>
     </Provider>
   );
